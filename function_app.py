cbd920876a4fd38615556f70ac2e364c40483824
Paul-Louis Pröve <mail@plpp.de>
2023-09-25 17:30:48 +0200

    removed all deps

--- function_app.py
+++ function_app.py
@@ -1,13 +1,13 @@
 import azure.functions as func
-from tokenizers import Tokenizer
-import onnxruntime as ort
-import numpy as np
+#from tokenizers import Tokenizer
+#import onnxruntime as ort
+#import numpy as np
 import logging
 import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-session = ort.InferenceSession("bge-base-en-v1.5.onnx")
-tokenizer = Tokenizer.from_file("tokenizer.json")
+#session = ort.InferenceSession("bge-base-en-v1.5.onnx")
+#tokenizer = Tokenizer.from_file("tokenizer.json")
 
 
 @app.route(route="embed")
@@ -19,16 +19,16 @@ def embed(req: func.HttpRequest) -> func.HttpResponse:
 
     embeddings = []
     logging.info(f"Processing {len(docs)} documents")
-    for doc in docs:
-        tok = tokenizer.encode(doc)
-        inp = {
-            "input_ids": np.array(tok.ids, dtype=np.int64)[None, ...],
-            "attention_mask": np.array(tok.attention_mask, dtype=np.int64)[None, ...],
-            "token_type_ids": np.array(tok.type_ids, dtype=np.int64)[None, ...],
-        }
-        embed = session.run(None, inp)[0][0, 0]
-        if req_body.get("normalize"):
-            embed = embed / np.linalg.norm(embed)
-        embeddings.append([round(x, 4) for x in embed.tolist()])
+    # for doc in docs:
+    #     tok = tokenizer.encode(doc)
+    #     inp = {
+    #         "input_ids": np.array(tok.ids, dtype=np.int64)[None, ...],
+    #         "attention_mask": np.array(tok.attention_mask, dtype=np.int64)[None, ...],
+    #         "token_type_ids": np.array(tok.type_ids, dtype=np.int64)[None, ...],
+    #     }
+    #     embed = session.run(None, inp)[0][0, 0]
+    #     if req_body.get("normalize"):
+    #         embed = embed / np.linalg.norm(embed)
+    #     embeddings.append([round(x, 4) for x in embed.tolist()])
 
-    return json.dumps(embeddings)
+    return json.dumps("test") #embeddings)
